9c0ea30dc0710de01c4b2430ead2b2c70bc08f3f
Alan <arob8086@uni.sydney.edu.au>
2021-07-26 23:08:34 +0000

    added basic gates

---
+++ src/gates/paulis.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+pauli_x = np.array([
+    [0, 1],
+    [1, 0]
+    ])
+
+pauli_y = np.array([
+    [0, -1j],
+    [1j,  0]
+    ])
+
+pauli_z = np.array([
+    [1,  0],
+    [0, -1]
+    ])  
