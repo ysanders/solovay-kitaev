a4480b92ef6f64388405472826c02fe247d64e8b
Yuval Sanders <yuval.rishu.sanders@gmail.com>
2021-07-27 16:14:48 +1000

    OK I didn't actually run the tests...

--- tests/sample_test.py
+++ tests/sample_test.py
@@ -1,14 +1,20 @@
 import unittest
 
-class TestTest:
-    # https://docs.python.org/3/library/unittest.html
-   
+class TestStringMethods(unittest.TestCase):
+
     def test_upper(self):
         self.assertEqual('foo'.upper(), 'FOO')
-    
+
     def test_isupper(self):
         self.assertTrue('FOO'.isupper())
         self.assertFalse('Foo'.isupper())
 
-if __name__ = "__main__":
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+if __name__ == "__main__":
     unittest.main()
