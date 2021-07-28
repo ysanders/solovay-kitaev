b1b563669dfdf8598a326dc4130bbac4becbb5bd
Yuval Sanders <yuval.rishu.sanders@gmail.com>
2021-07-28 20:29:11 +1000

    fixed notation for dawson_nielsen; still bugged

--- src/solovay_kitaev/dawson_nielsen.py
+++ src/solovay_kitaev/dawson_nielsen.py
@@ -1,23 +1,12 @@
-# this is just a template for [DN05]
-# see section 3 specifically...
-
-from numpy import prod
-from numpy import array
-
-def group_commutator(V, W):
-    return prod(prod(V, W), prod(V.conj().T, W.conj().T))
-
-def basic_approximation(U):
-    pass
-
-def GC_Decompose(U):
-    pass
+import numpy as np
+from solovay_kitaev.gc_decomposition import gc_decompose, dag
+from solovay_kitaev.basic_approximation import BaseCaseQuery
 
 def solovay_kitaev(
-        unitary: array,
+        unitary: np.ndarray,
         depth: int,
         gates = None : list # TODO import gates and include appropriate defaults, expand to *args
-        ) -> array, list:
+        ) -> np.ndarray, list:
     '''
         solovay_kitaev
         Function to implement the Solovay Kitaev algorithm
@@ -28,11 +17,18 @@ def solovay_kitaev(
         Returns the approximation of the unitary along with a 
         list of the gates that provide the approxmiation
     '''
+
+    if gates is None:
+        raise(NotImplementedError)
+
+    basic_approximation = BaseCaseQuery(*gates, depth=depth)
+
     if depth == 0:
-        return Basic_Approximation(unitary)
+        return basic_approximation(unitary)
     else:
-        previous_unitary = Solovay_Kitaev(unitary, depth-1)
-        V, W = GC_Decompose(prod(unitary, previous_unitary.conj().T))
-        prev_V = Solovay_Kitaev(V, depth-1)
-        prev_W = Solovay_Kitaev(W, depth-1)
-        return prod(group_commutator(prev_V, prev_W),  previous_unitary)
+        previous_unitary = solovay_kitaev(unitary, depth-1, gates=gates)
+        V, W = gc_decompose(unitary @ dag(previous_unitary))
+        prev_V = solovay_kitaev(V, *gates, depth - 1, gates=gates)
+        prev_W = solovay_kitaev(W, *gates, depth - 1, gates=gates)
+        group_commutator = V @ W @ dag(V) @ dag(W)
+        return group_commutator @ previous_unitary
