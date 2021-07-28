34cae68a187edbd37e8bd196831ef321e2461e77
Alan <arob8086@uni.sydney.edu.au>
2021-07-28 03:55:44 +0000

    updated test

--- tests/basic_approximation_test.py
+++ tests/basic_approximation_test.py
@@ -1,4 +1,6 @@
 import unittest
+from numpy import array, eye 
+
 from solovay_kitaev.basic_approximation import basic_approximation_generator
 from solovay_kitaev.gates import cliffords
 
@@ -13,7 +15,19 @@ class TestBasicApproximation(unittest.TestCase):
 
     def test_simple(self):
         gates = cliffords.clifford_generators
-        pass
+        gates.append(eye(2)) # Add the identity
+        base_cases = list(basic_approximation_generator(*gates, depth=1))
+
+        for case in base_cases:
+            gate_found = False
+            for gate in gates:
+                if (gate == case[0]).all():
+                    gate_found = True
+                    break
+            assert(gate_found)
+
+        return
+
             
 
 if __name__ == "__main__":
